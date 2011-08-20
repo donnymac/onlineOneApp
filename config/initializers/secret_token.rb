24ede024a9286c532634b7c2ffb7665fa08888ae
donnymac <don@onlineone.com.au>
2011-08-20 15:26:43 +1000

    initialise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OnlineOneApp::Application.config.secret_token = '79b5573c947d24961f95461d05adf803c736071ec9cc0b25215b0fd6b4f7e3c09c9ced6a884df0a670f028bf9d3f7212852d637a4b9f02ed3ec818d8cbefa09d'
